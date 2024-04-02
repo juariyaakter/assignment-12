6a31a3e488cab07fe7a07efaaf78858530fb66df
Juariya Akter <juariyaakter97@gmail.com>
2024-04-02 13:03:46 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var division = 12/2;
+
+console.log(division);
+
+
+var addition = 9+6;
+
+console.log(addition);
+
+let substraction = 10-2;
+
+console.log(substraction);
+
+{
+    var firstname="Juariya";
+       lastname="Akter";
+       fullname=firstname+" "+lastname
+        console.log(fullname)
+       
+}
+
+{let m = 10;
+    m++;
+    let o = m;
+    console.log(o)
+}
